a71a8ec6c10730058b296ed9ccc0915fea73eb72
Francisco Freire <franciscocfreire@gmail.com>
2022-10-19 09:30:09 -0300

    gas reporter - set false for now

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,12 +5,14 @@ require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("./task/block-number");
 require("./task/accounts");
+require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -22,11 +24,19 @@ module.exports = {
         },
         localhost: {
             url: "http://127.0.0.1:8545/",
-            chainId: 31337
-        }
+            chainId: 31337,
+        },
     },
     solidity: "0.8.17",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-reporter.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC"
     },
 };
